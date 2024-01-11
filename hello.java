42d3d5e63cdd66e3cabbd13ebbe010596032e7a4
RETHESHSHETTY <retheshahv@gmail.com>
2024-01-11 17:26:14 +0530

    committed by b1

--- hello.java
+++ hello.java
@@ -2,3 +2,11 @@ class hello{
 public int sum(int a,int b);
 public int diff(int a,int b);
 }
+class Studentimp implements Student{
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int sub(int a,int b){
+        return a-b;
+    }
+}
